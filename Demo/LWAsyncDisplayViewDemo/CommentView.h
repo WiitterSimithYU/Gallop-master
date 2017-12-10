d8b0076a41d6a61e7eae89f328ad31e99583b085
WiitterSimithYU <378961614@qq.com>
2017-12-10 11:49:59 +0800

    commit

---
+++ Demo/LWAsyncDisplayViewDemo/CommentView.h
@@ -0,0 +1,35 @@
+
+
+
+
+
+/********************* 有任何问题欢迎反馈给我 liuweiself@126.com ****************************************/
+/***************  https://github.com/waynezxcv/Gallop 持续更新 ***************************/
+/******************** 正在不断完善中，谢谢~  Enjoy ******************************************************/
+
+
+
+
+/**
+ *  弹出键盘框输入文字
+ */
+
+
+
+
+#import <UIKit/UIKit.h>
+#import "AutoFitSizeTextView.h"
+
+
+typedef void(^PressSendBlock)(NSString * content);
+
+@interface CommentView : UIView<AutoFitSizeTextViewDelegate>
+
+@property (nonatomic,strong) AutoFitSizeTextView* textView;
+@property (nonatomic,copy) NSString* placeHolder;
+@property (nonatomic,copy) PressSendBlock sendBlock;
+
+
+- (id)initWithFrame:(CGRect)frame sendBlock:(PressSendBlock)sendBlock;
+
+@end
